6f28bfcea2afeb6bfd4fce0cf774adc5017ae096
kimhanlim8107 <84323886+kimhanlim8107@users.noreply.github.com>
2021-06-22 20:17:06 +0900

    Create a route function that updates blog posts

--- blog/blog_write.py
+++ blog/blog_write.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import (
-    Blueprint, request, render_template, redirect, url_for
+    Blueprint, request, render_template, redirect, url_for, redirect
 )
 from blog.db import get_db
 
@@ -29,3 +29,28 @@ def write():
         
 
     return render_template('blog_write.html')
+
+@bp.route('/update/<repository>/<id>', methods=('GET', 'POST'))
+def update(repository, id):
+    db = get_db()
+    post = db.execute("SELECT * FROM post WHERE repository = '{}' AND id = '{}'".format(repository, id)).fetchone()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        repo = request.form['repository']
+        content = request.form['content']
+        date = datetime.now().strftime('%y-%m-%d %H:%M')
+        error = None
+        db = get_db()
+
+        if title == None:
+            error = 'title is required'
+        elif content == None:
+            error = 'content is required'
+
+        if error == None:
+            db.execute("UPDATE post SET title = ?, repository = ?, content = ?, date = ? WHERE id = ?", (title, repo, content, date, id))
+            db.commit()
+            return redirect(url_for('home.home'))
+
+    return render_template('blog_update.html', post=post)

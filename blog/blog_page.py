2c9b136b1b2017f63f5699d61d869f13867d611a
kimhanlim8107 <84323886+kimhanlim8107@users.noreply.github.com>
2021-06-25 23:18:38 +0900

    prevent URL injection using abort method

--- blog/blog_page.py
+++ blog/blog_page.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort
 from blog.db import get_db
 
-bp = Blueprint('page',__name__)
+bp = Blueprint('page', __name__)
 
 @bp.route('/<repository>/<id>')
 def page(repository, id):
     db = get_db()
     post = db.execute("SELECT * FROM post WHERE repository='{}' AND id='{}'".format(repository, id)).fetchone()
-    return render_template('blog_page.html', post=post)
+    if post == None:
+        abort(404)
+    else:
+        return render_template('blog_page.html', post=post)
